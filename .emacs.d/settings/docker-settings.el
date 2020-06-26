6daba0e2be8cd4a5f5cf19138cf5f05d90ff85f8
och6z <44880194+och6z@users.noreply.github.com>
2020-06-26 10:49:00 +0200

    Update docker-settings

--- .emacs.d/settings/docker-settings.el
+++ .emacs.d/settings/docker-settings.el
@@ -2,5 +2,25 @@
 ;; Docker settings ;;
 ;;;;;;;;;;;;;;;;;;;;;
 
+;; manage Docker from Emacs
+(use-package docker
+  :ensure t
+  :defer t
+  :bind ("C-c d" . docker))
+
+;; Emacs mode for handling Dockerfiles
+(use-package dockerfile-mode
+  :ensure t
+  :defer t
+  :init
+  (add-to-list 'auto-mode-alist '("Dockerfile[a-zA-Z.-]*\\'" . dockerfile-mode)))
+
+;; major mode for editing Docker-compose files
+(use-package docker-compose-mode
+  :ensure t
+  :defer t
+  :init
+  (add-to-list 'auto-mode-alist '("docker-compose.*\.yml\\'" . docker-compose-mode)))
+
 ;; add the symbol name to the features list
 (provide 'docker-settings)
