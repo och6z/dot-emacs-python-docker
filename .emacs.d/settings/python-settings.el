ca5722ad55bfda25bbcb22b2f75a3dcf3a2ef189
och6z <44880194+och6z@users.noreply.github.com>
2020-07-05 23:23:31 +0200

    Update python packages

--- .emacs.d/settings/python-settings.el
+++ .emacs.d/settings/python-settings.el
@@ -23,6 +23,9 @@
   :after python
   :init
   (add-hook 'python-mode-hook 'elpy-mode)
+  ;; (add-hook 'elpy-mode-hook (lambda ()
+  ;;                             (add-hook 'before-save-hook
+  ;;                                       'elpy-black-fix-code nil t)))
   :load-path "~/.emacs.d/elpa/elpy-[0-9.]*\\'"
   :config
   (setq elpy-rpc-python-command "python"
@@ -34,18 +37,41 @@
   :ensure t
   :after python
   :config
-  (setq py-autopep8-options '("--max-line-length=100"))
+  (setq py-autopep8-options '(
+                              "--max-line-length=72"
+                              "--aggressive"
+                              "--aggressive"
+                              ))
+  ;; :init
+  ;; (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
+  )
+
+;; python black package to reformat python buffers
+(use-package blacken
+  :ensure t
+  :after python
   :init
-  (add-hook 'python-mode-hook 'py-autopep8-enable-on-save))
+  (add-hook 'python-mode-hook 'blacken-mode)
+  :config
+  (setq blacken-line-length 88
+        blacken-skip-string-normalization t
+        blacken-fast-unsafe t)
+  )
+
+;; yapf to format python buffer
+(use-package yapfify
+  :ensure t
+  :after python)
 
 ;; use isort to sort the imports in a python buffer
 (use-package py-isort
   :ensure t
   :after python
-  :config
-  (setq py-isort-options '("--lines=100"))
+  ;; :config
+  ;; (setq py-isort-options '("--multi-line 3"))
   :init
-  (add-hook 'before-save-hook 'py-isort-before-save))
+  (add-hook 'before-save-hook 'py-isort-before-save)
+  )
 
 ;; add the symbol name to the features list
 (provide 'python-settings)
